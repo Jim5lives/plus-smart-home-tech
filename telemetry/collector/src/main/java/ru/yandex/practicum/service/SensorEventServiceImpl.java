21a5125f738cf6f40001ca2393ad32d5b91f6ea6
Vyacheslav Tetyuev <s1995t@yandex.ru>
2024-12-08 15:01:57 +0500

    add GeneralKafkaSerializer

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventServiceImpl.java
@@ -2,13 +2,20 @@ package ru.yandex.practicum.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.kafkaClient.KafkaClient;
 import ru.yandex.practicum.model.SensorEvent;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SensorEventServiceImpl implements SensorEventService {
+    private final KafkaClient kafkaClient;
+
+    @Value(value = "${sensorEventTopic}")
+    private String topic;
+
     @Override
     public void collect(SensorEvent sensorEvent) {
 
