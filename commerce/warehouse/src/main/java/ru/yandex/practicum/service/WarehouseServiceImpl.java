15f4b886d3884252d86c0151c5434a0ad609fe39
Vyacheslav Tetyuev <s1995t@yandex.ru>
2025-01-17 23:16:50 +0500

    add addProductToWarehouse() method

--- commerce/warehouse/src/main/java/ru/yandex/practicum/service/WarehouseServiceImpl.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/service/WarehouseServiceImpl.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.exception.SpecifiedProductAlreadyInWarehouseException;
 import ru.yandex.practicum.mapper.WarehouseProductMapper;
+import ru.yandex.practicum.model.WarehouseProduct;
 import ru.yandex.practicum.repository.WarehouseRepository;
 import ru.yandex.practicum.request.NewProductInWarehouseRequest;
+
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -19,6 +23,17 @@ public class WarehouseServiceImpl implements WarehouseService {
 
     @Override
     public void addNewProductToWarehouse(NewProductInWarehouseRequest request) {
+        checkIfProductAlreadyInWarehouse(request.getProductId());
+        WarehouseProduct product = warehouseProductMapper.mapToWarehouseProduct(request);
+        warehouseRepository.save(product);
+        log.info("Product is added to warehouse: {}", product);
+    }
 
+    private void checkIfProductAlreadyInWarehouse(UUID id) {
+        warehouseRepository.findById(id)
+                .ifPresent(product -> {
+                    log.warn("Product with ID: {} already exists", id);
+                    throw new SpecifiedProductAlreadyInWarehouseException("Product is already in warehouse");
+                });
     }
 }
